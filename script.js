a23c46ad193ba032006273d1d79165ac00e7c1b1
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2021-07-09 22:32:35 -0300

    Update script.js
    Nesse script fiz o código de um sistema de notas em javascript

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let nota2 = 5;
 let nota3 = 8;
 let nota4 = 6;
 let media = (nota1 + nota2 + nota3 + nota4) / 4;
-
-
 if (media <= 10 || media >=8){
     console.log("Conceito A");
     console.log("Aluno aprovado");
